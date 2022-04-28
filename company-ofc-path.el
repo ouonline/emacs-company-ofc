62adc4f6763b7f4ef248c5f3eac81bd870381b39
Guoyu Ou <benogy@gmail.com>
2022-04-28 18:18:14 +0800

    rename internal functions

--- company-ofc-path.el
+++ company-ofc-path.el
@@ -20,22 +20,22 @@
 ;; -----------------------------------------------------------------------------
 ;; struct definitions
 
-(cl-defstruct company-ofc-path-candidate-s parent-dir entry-list)
+(cl-defstruct company-ofc--path-candidate-s parent-dir entry-list)
 
 ;; -----------------------------------------------------------------------------
 ;; global variales
 
-(defvar company-ofc-path-candidate-stack '())
-(defvar company-ofc-path-real-prefix "") ;; updated when `prefix` is called
+(defvar company-ofc--path-candidate-stack '())
+(defvar company-ofc--path-real-prefix "") ;; updated when `prefix` is called
 
 ;; -----------------------------------------------------------------------------
 
-(defun company-ofc-path-generic-list-find (list predicate)
+(defun company-ofc--path-generic-list-find (list predicate)
   (cl-dolist (element list)
     (if (funcall predicate element)
         (cl-return element))))
 
-(defun company-ofc-path-do-fuzzy-compare (pattern pattern-length text text-length)
+(defun company-ofc--path-do-fuzzy-compare (pattern pattern-length text text-length)
   "tells if `pattern` is part of `text`"
   (if (> pattern-length text-length)
       nil
@@ -48,28 +48,28 @@
         (cl-incf text-idx))
       (= pattern-idx pattern-length))))
 
-(defun company-ofc-path-grab-prefix (pattern)
+(defun company-ofc--path-grab-prefix (pattern)
   (when (looking-back pattern (line-beginning-position) t)
     (let ((prefix (match-string 0)))
       (when (string-match-p "/" prefix)
         ;; returns a pseudo prefix to make tooltip shown at the proper position and
-        ;; stores the real prefix in `company-ofc-path-real-prefix`
+        ;; stores the real prefix in `company-ofc--path-real-prefix`
         (if (string-prefix-p "~/" prefix)
-            (setq company-ofc-path-real-prefix (concat (substitute-in-file-name "$HOME/")
-                                                       (substring-no-properties prefix 2)))
-          (setq company-ofc-path-real-prefix prefix))
+            (setq company-ofc--path-real-prefix (concat (substitute-in-file-name "$HOME/")
+                                                        (substring-no-properties prefix 2)))
+          (setq company-ofc--path-real-prefix prefix))
         (file-name-nondirectory prefix)))))
 
-(defun company-ofc-path-find-entry-list-in-stack (parent-dir)
-  (company-ofc-path-generic-list-find company-ofc-path-candidate-stack
-                                      (lambda (candidate)
-                                        (let ((dir (company-ofc-path-candidate-s-parent-dir candidate)))
-                                          (string= dir parent-dir)))))
+(defun company-ofc--path-find-entry-list-in-stack (parent-dir)
+  (company-ofc--path-generic-list-find company-ofc--path-candidate-stack
+                                       (lambda (candidate)
+                                         (let ((dir (company-ofc--path-candidate-s-parent-dir candidate)))
+                                           (string= dir parent-dir)))))
 
-(defun company-ofc-path-get-entry-list (parent-dir)
-  (let ((candidate (company-ofc-path-find-entry-list-in-stack parent-dir)))
+(defun company-ofc--path-get-entry-list (parent-dir)
+  (let ((candidate (company-ofc--path-find-entry-list-in-stack parent-dir)))
     (if candidate
-        (company-ofc-path-candidate-s-entry-list candidate)
+        (company-ofc--path-candidate-s-entry-list candidate)
       (let ((current-entry-list (directory-files parent-dir)))
         (if current-entry-list
             (let ((new-entry-list (mapcar (lambda (entry)
@@ -78,37 +78,37 @@
                                                   (concat entry "/")
                                                 entry)))
                                           current-entry-list)))
-              (push (make-company-ofc-path-candidate-s :parent-dir parent-dir
-                                                       :entry-list new-entry-list)
-                    company-ofc-path-candidate-stack)
+              (push (make-company-ofc--path-candidate-s :parent-dir parent-dir
+                                                        :entry-list new-entry-list)
+                    company-ofc--path-candidate-stack)
               new-entry-list)
           '())))))
 
-(defun company-ofc-path-find-candidate (not-used)
-  (let ((parent-dir (file-name-directory company-ofc-path-real-prefix))
-        (last-component (downcase (file-name-nondirectory company-ofc-path-real-prefix))))
+(defun company-ofc--path-find-candidate (not-used)
+  (let ((parent-dir (file-name-directory company-ofc--path-real-prefix))
+        (last-component (downcase (file-name-nondirectory company-ofc--path-real-prefix))))
     (when (file-directory-p parent-dir)
-      (let ((entry-list (company-ofc-path-get-entry-list parent-dir)))
+      (let ((entry-list (company-ofc--path-get-entry-list parent-dir)))
         (when (not (null entry-list))
           (let ((last-component-length (length last-component))
                 (entry-result '()))
             (dolist (entry entry-list)
-              (when (company-ofc-path-do-fuzzy-compare last-component last-component-length
-                                                       (downcase entry) (length entry))
+              (when (company-ofc--path-do-fuzzy-compare last-component last-component-length
+                                                        (downcase entry) (length entry))
                 (push entry entry-result)))
             entry-result))))))
 
-(defun company-ofc-path-post-completion (token)
+(defun company-ofc--path-post-completion (token)
   (when (string-suffix-p "/" token)
     (delete-char (- 1)))
-  (setq company-ofc-path-candidate-stack '()))
+  (setq company-ofc--path-candidate-stack '()))
 
 (defun company-ofc-path (command &optional arg &rest ignored)
   (interactive (list 'interactive))
   (cl-case command
-    (prefix (company-ofc-path-grab-prefix company-ofc-path-token-pattern))
-    (candidates (company-ofc-path-find-candidate arg))
-    (post-completion (company-ofc-path-post-completion arg))
+    (prefix (company-ofc--path-grab-prefix company-ofc-path-token-pattern))
+    (candidates (company-ofc--path-find-candidate arg))
+    (post-completion (company-ofc--path-post-completion arg))
     (sorted t)
     (no-cache t)))
 
